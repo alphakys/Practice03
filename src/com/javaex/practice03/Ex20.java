85146ec350a36e01480625e47cafb5814b368b5a
'alphakys' <'gys1120@naver.com'>
2020-11-17 18:50:13 +0900

    11.17 Ex20 수정

--- src/com/javaex/practice03/Ex20.java
+++ src/com/javaex/practice03/Ex20.java
@@ -10,18 +10,24 @@ public class Ex20 {
 		
 	
 		
-		boolean run = true;
+		
 		String choice;
 	
 		int answer = 10;
 		
 		
+		boolean run = true;
 		
-		while(run) {
-			
+		
 			System.out.println("====================================");
 			System.out.println("[      숫자맞추기 게임시작       ]");
 			System.out.println("====================================");
+		
+			
+			
+			while(run) {
+			
+		
 	
 		
 		
@@ -33,41 +39,50 @@ public class Ex20 {
 		if(num>answer)
 					{
 					System.out.println("더 낮게");
+					
 					}
 		
 		else if(num==answer)
 					{		
 					System.out.println("맞았습니다");	
-					run = false;
+					System.out.print("게임을 종료하시겠습니까?(y/n)  >>");
+					
+					sc.nextLine();
+					 choice = sc.nextLine();	
+					 	
+					 	
+					
+					if(choice.equals("y")) 
+							{
+							System.out.println("====================================");
+							System.out.println("[      숫자맞추기 게임종료       ]");
+							System.out.println("====================================");
+							run=false;
+							}
+					else {
+							run =true; 
+							
+					 	 }	
+					
 					} 
+		
+		
 		else 		{
 					System.out.println("더 높게");
+				
 					}
 
+			
+		
+		
+			
+			
+			
 					}		
 			
-		System.out.print("게임을 종료하시겠습니까?(y/n)  >>");
-		
-		sc.nextLine();
-		 choice = sc.nextLine();	
-		 	
-		 	
-		
-		if(choice.equals("y")) 
-				{
-				System.out.println("====================================");
-				System.out.println("[      숫자맞추기 게임종료       ]");
-				System.out.println("====================================");
-				}
-		else {
-				run =true;
-		 	 }	
-		
+
 		 
-			
-		
-		
-			
+
 			
 			
 			
