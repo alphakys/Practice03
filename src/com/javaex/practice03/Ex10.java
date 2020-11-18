3f1b5a432ac069abdf2d47bb99649176608f3ccc
'alphakys' <'gys1120@naver.com'>
2020-11-18 12:18:39 +0900

    11.18

--- src/com/javaex/practice03/Ex10.java
+++ src/com/javaex/practice03/Ex10.java
@@ -11,29 +11,54 @@ public class Ex10 {
 		
 		System.out.println("숫자를 입력하세요");
 		
-		System.out.println("숫자: ");
+		System.out.print("숫자: ");
 		int num1 = sc.nextInt();
 		
-		System.out.println("숫자: ");
+		System.out.print("숫자: ");
 		int num2 = sc.nextInt();
 		
-		System.out.println("숫자: ");
+		System.out.print("숫자: ");
 		int num3 = sc.nextInt();
 		
-		System.out.println("숫자: ");
+		System.out.print("숫자: ");
 		int num4 = sc.nextInt();
 		
-		System.out.println("숫자: ");
+		System.out.print("숫자: ");
 		int num5 = sc.nextInt();
 		
 		
 		
+		int[] Max = {num1,num2,num3,num4,num5};
+		
+		int max = Max[0];
+		
+		
+		for(int i=0;i<Max.length;i++) 
+						{
+						if(max<Max[i]) {
+										max=Max[i];
+										}
+						}System.out.println(max);
 		
 		
 		
 		
 		
-		sc.close();
+		
+						
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		
 		
 		
